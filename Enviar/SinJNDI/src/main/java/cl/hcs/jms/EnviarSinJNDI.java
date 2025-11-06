189c6ad0d01f659ccd96f325e436dda396387b57
hcslearning <learning@hardcybersoft.cl>
2025-11-06 03:14:13 -0300

    restructurando multiproyecto segun buenas practicas

---
+++ Enviar/SinJNDI/src/main/java/cl/hcs/jms/EnviarSinJNDI.java
@@ -0,0 +1,8 @@
+package cl.hcs.jms;
+
+public class EnviarSinJNDI {
+
+    public static void main(String[] args) {
+        System.out.println("prueba Enviar sin JNDI");
+    }
+}
---
+++ Recibir/Topicos/NoDurableCompartido/src/main/java/cl/hcs/jms/EnviarConJNDI.java
@@ -0,0 +1,9 @@
+package cl.hcs.jms;
+
+public class EnviarConJNDI {
+
+    public static void main(String[] args) {
+        System.out.println("prueba Enviar con JNDI");
+    }
+
+}
